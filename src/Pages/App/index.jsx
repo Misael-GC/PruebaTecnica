8c5c49b07fd195a31be18e2f8526da38c519c1ff
Misael-GC <misael.gomez1623@gmail.com>
2024-03-26 09:54:17 -0600

    enrutamiento

--- src/Pages/App/index.jsx
+++ src/Pages/App/index.jsx
@@ -1,21 +1,28 @@
+import { useRoutes, BrowserRouter } from 'react-router-dom';
 import Promociones from '../Promociones';
 import Nosotros from '../Nosotros';
 import Productos from '../Productos';
 import Usuario from '../Usuario';
+import NotFound from '../NotFound';
+import './App.css';
 
-import './App.css'
+const AppRoutes = () => {
+  let routes = useRoutes([
+    { path: '/', element: <Promociones /> },
+    { path: '/nosotros', element: <Nosotros /> },
+    { path: '/productos', element: <Productos /> },
+    { path: '/usuario', element: <Usuario /> },
+    { path: '/*', element: <NotFound /> },
+  ])
+  return routes;
+}
 
 function App() {
 
   return (
-    <>
-      <div className='bg-zinc-400'>
-        <Promociones/>
-        <Nosotros/>
-        <Productos/>
-        <Usuario/>
-      </div>
-    </>
+    <BrowserRouter>
+      <AppRoutes/>
+    </BrowserRouter>
   )
 }
 
