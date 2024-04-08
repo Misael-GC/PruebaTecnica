83e6c33e9e5635b73379fa13080f47fd3a3592f4
Misael-GC <misael.gomez1623@gmail.com>
2024-04-07 23:54:56 -0600

    Api interna

--- src/Components/Card/index.jsx
+++ src/Components/Card/index.jsx
@@ -1,54 +1,42 @@
-import './index.css';
-import facebookIcon from '../../Assets/Icons/Vectorfa.png';
-import twitterIcon from '../../Assets/Icons/Vectort.png';
-import instagramIcon from '../../Assets/Icons/VectorIns.png';
-import productImage from '../../Assets/Images/bote.png';
-import ratingIcon from '../../Assets/Icons/Rating.svg';
 
-function Card() {
+import './index.css';
+
+function Card({ product }) {
+  const { name, image, description, price, discountPrice, quantity, ratingIcon } = product;
+
   return (
     <div className="card">
-
       <div className="card__image-container">
         <figure>
-          <img src={productImage} alt="Product" />
+          <img src={image} alt={name} />
         </figure>
 
         <div className='card__heart bg-red-500 w-8'>00</div>
         <div className='card__discount bg-red-300 w-8'>00</div>
-
+        {/* Resto del contenido de la tarjeta */}
         <div className="card__details-container">
           <div className="card__details">
-            <p className="">Citrate Mag </p>
-            <p className="">240 Capsulas 800 Mg</p>
-            <figure className="card__score m-3">
-              <img src={ratingIcon} alt='Rating'/>
-            </figure>
+            <p>{name}</p>
+            <p>{description}</p>
           </div>
-          
           <div className="card__price">
-            <p>$273.00</p>
-            <p>--$134.00--</p>
+            <p>${price}</p>
+            <p>--${discountPrice}--</p>
           </div>
         </div>
       </div>
-      
       <div className="card__buttons-container">
+        {/* Botones, iconos de redes sociales, etc. */}
         <div className="card__social-container">
           <p>Comparte</p>
-          <figure className="card__social-icons"> 
-            <img src={facebookIcon} alt='Facebook' className="social-icon"></img>
-            <img src={twitterIcon} alt='Twitter' className="social-icon"></img>
-            <img src={instagramIcon} alt='Instagram' className="social-icon"></img>
-          </figure>
+          {/* Iconos de redes sociales aquí */}
         </div>
-        
         <div className="card__add-button">
           Agregar
         </div>
       </div>
     </div>
-  )
+  );
 }
 
 export default Card;
--- src/Pages/Promociones/index.jsx
+++ src/Pages/Promociones/index.jsx
@@ -1,14 +1,15 @@
 
 import Layout from '../../Components/Layout'
 import Banner from '../../Components/Banner'
-import Card from '../../Components/Card'
-
+//import Card from '../../Components/Card'
+import CardContainer from '../../Components/CardContainer/index';
 
 function Promociones() {
   return (
     <Layout>
       <Banner/>
-      <Card/>
+      {/* <Card/> */}
+      <CardContainer />
     </Layout>
   )
 }
